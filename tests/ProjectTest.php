edc2e336e80526cd32efffe6d8009eaf5d69c5cb
Brooke Bryan <brooke@bajb.net>
2014-09-09 00:18:55 +0100

    Code coverage using new testing tools

--- tests/ProjectTest.php
+++ tests/ProjectTest.php
@@ -1,16 +1,45 @@
 <?php
+namespace Tests;
 
-class ProjectTest extends PHPUnit_Framework_TestCase
+use Cubex\Testing\CubexTestCase;
+use Cubex\View\Layout;
+
+class ProjectTest extends CubexTestCase
 {
   public function testProjectIsCubexKernel()
   {
-    $cubex = new \Cubex\Cubex(__DIR__);
-    $cubex->prepareCubex();
-    $cubex->processConfiguration($cubex->getConfiguration());
-
     $this->assertInstanceOf(
       '\Cubex\Kernel\CubexKernel',
-      $cubex->make('\Cubex\Kernel\CubexKernel')
+      $this->getCubex()->make('\Cubex\Kernel\CubexKernel')
     );
+  }
+
+  public function testDefaultAction()
+  {
+    $this->getResponse('/');
+    $this->assertResponseContains('Welcome to Cubex');
+    $this->assertResponseContains('framework and build something awesome');
+    $this->assertResponseOk();
+    $this->assertReturnsCubexResponse();
+    $this->assertReturnsViewModel();
+
+    $original = $this->getLastResponse()->getOriginal();
+    if($original instanceof Layout)
+    {
+      $view = $original->get('content');
+      $this->assertInstanceOf('\Skeleton\Application\Views\WelcomeView', $view);
+    }
+  }
+
+  public function testHello()
+  {
+    $this->getResponse('/hello');
+    $this->assertResponseContains('Hello World');
+  }
+
+  public function testHi()
+  {
+    $this->getResponse('/hi');
+    $this->assertRedirectedTo('/hello/world');
   }
 }
